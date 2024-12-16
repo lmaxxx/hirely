c2d5ff3da0b898638fddd72183173eb4fb01e35c
Maksym Leshchenko <leshchenko.maksym10@gmail.com>
2024-12-17 00:50:21 +0100

    feat(application-settings): create rich editor and use it in application settings

---
+++ src/components/rich-editor.tsx
@@ -0,0 +1,114 @@
+import {useEffect} from "react";
+import {useEditor, EditorContent} from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import BulletList from "@tiptap/extension-bullet-list";
+import OrderedList from "@tiptap/extension-ordered-list";
+import Heading from "@tiptap/extension-heading";
+import {Button} from "@/components/ui/button.tsx";
+
+type Props = {
+  content: string;
+  setContent: (content: string) => void;
+  valueToReset?: string
+}
+
+export default function RichEditor({content, setContent, valueToReset}: Props) {
+  const editor = useEditor({
+    extensions: [
+      StarterKit,
+      BulletList,
+      OrderedList,
+      Heading.configure({
+        levels: [1, 2, 3],
+      }),
+    ],
+    content
+  });
+
+  useEffect(() => {
+    if (editor) {
+      editor.on("update", () => {
+        setContent(editor.getHTML());
+      });
+    }
+  }, [editor]);
+
+  useEffect(() => {
+    if(content === valueToReset) {
+      editor?.commands.setContent(content);
+    }
+  }, [content]);
+
+  if (!editor) return null;
+
+  return (
+    <div className={"max-w-2xl"}>
+      <div className={"flex gap-1 mb-2 items-center"}>
+        <p>Description:</p>
+        <Button
+          variant={"outline"}
+          type={"button"}
+          onClick={() => editor.chain().focus().toggleHeading({level: 1}).run()}
+          className={`${editor.isActive("heading", {level: 1}) && "font-semibold bg-muted"}`}
+        >
+          H1
+        </Button>
+        <Button
+          variant={"outline"}
+          type={"button"}
+          onClick={() => editor.chain().focus().toggleHeading({level: 2}).run()}
+          className={editor.isActive("heading", {level: 2}) && "font-semibold bg-muted"}
+        >
+          H2
+        </Button>
+        <Button
+          variant={"outline"}
+          type={"button"}
+          onClick={() => editor.chain().focus().toggleHeading({level: 3}).run()}
+          className={editor.isActive("heading", {level: 3}) && "font-semibold bg-muted"}
+        >
+          H3
+        </Button>
+        <Button
+          variant={"outline"}
+          type={"button"}
+          onClick={() => editor.chain().focus().toggleBold().run()}
+          className={editor.isActive("bold") && "font-semibold bg-muted"}
+        >
+          Bold
+        </Button>
+
+        <Button
+          variant={"outline"}
+          type={"button"}
+          onClick={() => editor.chain().focus().toggleItalic().run()}
+          className={editor.isActive("italic") && "font-semibold bg-muted"}
+        >
+          Italic
+        </Button>
+
+        <Button
+          variant={"outline"}
+          type={"button"}
+          onClick={() => editor.chain().focus().toggleBulletList().run()}
+          className={editor.isActive("bulletList") && "font-semibold bg-muted"}
+        >
+          Bullet List
+        </Button>
+
+        <Button
+          variant={"outline"}
+          type={"button"}
+          onClick={() => editor.chain().focus().toggleOrderedList().run()}
+          className={editor.isActive("orderedList") && "font-semibold bg-muted"}
+        >
+          Ordered List
+        </Button>
+      </div>
+
+      <div className={"editor-container"}>
+        <EditorContent editor={editor}/>
+      </div>
+    </div>
+  );
+};
--- src/features/application-settings/components/edit-application-settings-form.tsx
+++ src/features/application-settings/components/edit-application-settings-form.tsx
@@ -1,6 +1,5 @@
 import {Form, FormControl, FormField, FormItem, FormMessage} from "@/components/ui/form.tsx";
 import {Input} from "@/components/ui/input.tsx";
-import {EditorContent, useEditor} from "@tiptap/react";
 import {Button} from "@/components/ui/button.tsx";
 import {Loader2} from "lucide-react";
 import {
@@ -11,13 +10,12 @@ import {toast} from "react-toastify";
 import {useForm} from "react-hook-form";
 import {zodResolver} from "@hookform/resolvers/zod";
 import useHandleRequest from "@/hooks/use-handle-request.tsx";
-import StarterKit from "@tiptap/starter-kit";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {useParams} from "react-router";
 import {Application} from "@/entities.type.ts";
 import {updateApplicationById} from "@/features/applications/service.ts";
+import RichEditor from "@/components/rich-editor.tsx";
 
-const extensions = [StarterKit]
 
 type Props = {
   application: Application
@@ -25,12 +23,9 @@ type Props = {
 }
 
 export default function EditApplicationSettingsForm({application, setApplication}: Props) {
+  const [descriptionContent, setDescriptionContent] = useState("");
   const {applicationId} = useParams();
   const {run, isLoading} = useHandleRequest();
-  const editor = useEditor({
-    extensions,
-    content: application.description ?? ""
-  })
   const form = useForm({
     resolver: zodResolver(editApplicationSettings),
     defaultValues: {
@@ -40,14 +35,14 @@ export default function EditApplicationSettingsForm({application, setApplication
   form.watch("position")
 
   const isNewData = () => {
-    return application.position !== form.getValues("position") || editor?.getHTML() !== application.description;
+    return application.position !== form.getValues("position") || descriptionContent !== application.description;
   }
 
   const onSubmit = (values: EditApplicationSettingsValues) => run(
     async () => {
       const updatedApplication = await updateApplicationById(
         +applicationId!,
-        {...values, description: editor?.getHTML()}
+        {...values, description: descriptionContent}
       )
       toast.success("Application was updated successfully.");
       setApplication(updatedApplication)
@@ -56,12 +51,13 @@ export default function EditApplicationSettingsForm({application, setApplication
 
   const onReset = () => {
     form.setValue("position", application.position);
-    editor?.commands.setContent(application.description)
+    setDescriptionContent(application.description)
   }
 
   useEffect(() => {
     if (application) {
       form.setValue("position", application.position);
+      setDescriptionContent(application.description)
     }
   }, [application]);
 
@@ -80,9 +76,14 @@ export default function EditApplicationSettingsForm({application, setApplication
             </FormItem>
           )}
         />
-        <div className={"editor-container"}>
-          <EditorContent editor={editor}/>
-        </div>
+        {
+          descriptionContent &&
+          <RichEditor
+            content={descriptionContent}
+            setContent={setDescriptionContent}
+            valueToReset={application.description}
+          />
+        }
         <div className={"flex gap-2"}>
           <Button type="submit" disabled={isLoading || !isNewData()}>
             {isLoading && <Loader2 className="animate-spin mr-2 h-4 w-4"/>}
